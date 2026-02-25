560ac5cdcf17378b3fc5f589a7678c08fb630d87
selva7378 <selvaganesh7378@gmail.com>
2026-02-25 18:05:32 +0530

    created new design module and set up theme

--- designsystem/build.gradle.kts
+++ designsystem/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -36,7 +37,18 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.compose.ui.tooling.preview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
--- designsystem/src/main/java/com/example/designsystem/theme/Color.kt
+++ designsystem/src/main/java/com/example/designsystem/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.wetherprototype.ui.theme
+package com.example.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
